bd0d9016c06ff4cf60d7a745501428763016b0dd
Marek Pikosz <marek.pikosz@gmail.com>
2019-03-13 15:09:26 +0100

    Main component styling

--- src/components/Main/Main.js
+++ src/components/Main/Main.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 import Join from './../Join/Join'
+import styles from './Main.module.scss'
 
 function Main() {
   return (
-    <main>
+    <main className={styles.Main}>
       <Switch>
         <Route exact path='/' component={Join}/>
       </Switch>
