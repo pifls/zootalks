e627acbc6952d4bbf0490d0a58bdf53db4917045
Marek Pikosz <marek.pikosz@gmail.com>
2019-03-13 13:52:19 +0100

    Add SCSS and modules + breakpoints

--- src/App.js
+++ src/App.js
@@ -1,16 +1,16 @@
 import React, { Component } from 'react';
 import Navbar from './components/Navbar/Navbar'
 import Main from './components/Main/Main'
+import styles from './App.module.scss'
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
+      <div className={styles.App}>
         <Navbar/>
         <Main/>
       </div>
     );
   }
 }
-
 export default App;
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
-
+import styles from './Navbar.module.scss'
 function Navbar() {
   return (
-    <div className="Navbar">
+    <div className={styles.Navbar}>
       <Link to='/'>zootalks</Link>
     </div>
   )
