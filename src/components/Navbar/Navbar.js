d2720c80918610a33744bc7c24e54bcaeede416b
Marek Pikosz <marek.pikosz@gmail.com>
2019-03-13 14:38:30 +0100

    Navbar added

--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,10 +1,13 @@
 import React from 'react'
 import { Link } from 'react-router-dom'
 import styles from './Navbar.module.scss'
+import logo from './../../assets/logo.png'
 function Navbar() {
   return (
     <div className={styles.Navbar}>
-      <Link to='/'>zootalks</Link>
+      <Link className={styles.Logo} to='/'>
+        <img src={ logo } alt="logo with a lion"/>
+      </Link>
     </div>
   )
 }
